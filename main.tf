2523feb319437b184137c3ec913545d1a15d1bd8
Nick Selpa <nickselpa@gmail.com>
2019-01-13 22:53:29 -0500

    Completed "Interpolation" Lesson

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
 # Download the latest Ghost image
-resource "docker_image" "linux_academy" {
+resource "docker_image" "la_docker_image" {
     name = "ghost:latest"
+}
+
+# Start the Container
+resource "docker_container" "la_docker_container" {
+    name = "blog"
+    image = "${docker_image.la_docker_image.latest}"
+    ports {
+        internal = "2368"
+        external = "80"
+    }
 }
